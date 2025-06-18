81c790fd0abf3b23551614f13c92042964920583
tiagoluis19 <tiagoluisfurtado@gmail.com>
2025-06-18 19:13:27 +0100

    Correct main function

--- gauntlet/src/main.cpp
+++ gauntlet/src/main.cpp
@@ -1,47 +1,154 @@
 #include <Arduino.h>
-#include "Arduino_LSM9DS1.h"
+#include <LSM9DS1.h>
 
-//leitura dos dados GUARDAR O TERMINAL EM CSV
+#include <Picovoice_EN.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println("Hello, World!");
-  IMU.begin();
-  //4 pinos input dos halls
-  //pinos para led laser e power bank module
+#include "params.h"
 
-  Serial.println("Timestamp,Accel_X,Accel_Y,Accel_Z,Gyro_X,Gyro_Y,Gyro_Z");
+#define MEMORY_BUFFER_SIZE (70 * 1024)
+
+static const char *ACCESS_KEY = "DLpSOMDCfmdoqWzfa/dFjuWtXfntd98N1+gHSXPSxPuAOOqZYZN2tg=="; // AccessKey string obtained from Picovoice Console (https://picovoice.ai/console/)
+
+static pv_picovoice_t *handle = NULL;
+
+static int8_t memory_buffer[MEMORY_BUFFER_SIZE] __attribute__((aligned(16)));
+
+static const float PORCUPINE_SENSITIVITY = 0.5f;
+static const float RHINO_SENSITIVITY = 0.5f;
+static const float RHINO_ENDPOINT_DURATION_SEC = 1.0f;
+static const bool RHINO_REQUIRE_ENDPOINT = true;
+
+static void wake_word_callback(void) {
+    Serial.println("Wake word detected!");
+    
 }
 
+static void inference_callback(pv_inference_t *inference) {
+    Serial.println("{");
+    Serial.print("    is_understood : ");
+    Serial.println(inference->is_understood ? "true" : "false");
+    if (inference->is_understood) {
+        Serial.print("    intent : ");
+        Serial.println(inference->intent);
+        if (inference->num_slots > 0) {
+            Serial.println("    slots : {");
+            for (int32_t i = 0; i < inference->num_slots; i++) {
+                Serial.print("        ");
+                Serial.print(inference->slots[i]);
+                Serial.print(" : ");
+                Serial.println(inference->values[i]);
+            }
+            Serial.println("    }");
+        }
+    }
+    Serial.println("}\n");
+    pv_inference_delete(inference);
+}
 
+static void print_error_message(char **message_stack, int32_t message_stack_depth) {
+    for (int32_t i = 0; i < message_stack_depth; i++) {
+        Serial.println(message_stack[i]);
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    while (!Serial);
+    
+    Serial.println("Picovoice EN Example");
+    pinMode(LEDR, OUTPUT);
+    pinMode(LEDG, OUTPUT); 
+    pinMode(LEDB, OUTPUT); 
+
+
+    digitalWrite(LEDR, 1);
+
+    delay(1000); // Wait for 1 second to ensure the serial connection is established
+
+    pv_status_t status = pv_audio_rec_init();
+    if (status != PV_STATUS_SUCCESS) {
+        Serial.print("Audio init failed with ");
+        Serial.println(pv_status_to_string(status));
+        while (1);
+    }
+
+    char **message_stack = NULL;
+    int32_t message_stack_depth = 0;
+    pv_status_t error_status;
+
+    status = pv_picovoice_init(
+        ACCESS_KEY,
+        MEMORY_BUFFER_SIZE,
+        memory_buffer,
+        sizeof(KEYWORD_ARRAY),
+        KEYWORD_ARRAY,
+        PORCUPINE_SENSITIVITY,
+        wake_word_callback,
+        sizeof(CONTEXT_ARRAY),
+        CONTEXT_ARRAY,
+        RHINO_SENSITIVITY,
+        RHINO_ENDPOINT_DURATION_SEC,
+        RHINO_REQUIRE_ENDPOINT,
+        inference_callback,
+        &handle);
+    if (status != PV_STATUS_SUCCESS) {
+        Serial.print("Picovoice init failed with ");
+        Serial.println(pv_status_to_string(status));
+
+        error_status = pv_get_error_stack(&message_stack, &message_stack_depth);
+        if (error_status != PV_STATUS_SUCCESS) {
+            Serial.println("Unable to get Porcupine error state");
+            while (1);
+        }
+        print_error_message(message_stack, message_stack_depth);
+        pv_free_error_stack(message_stack);
+
+        while (1);
+    }
+
+    const char *rhino_context = NULL;
+    status = pv_picovoice_context_info(handle, &rhino_context);
+    if (status != PV_STATUS_SUCCESS) {
+        Serial.print("retrieving context info failed with");
+        Serial.println(pv_status_to_string(status));
+        while (1);
+    }
+    Serial.println("Wake word: 'hey computer'");
+    Serial.println(rhino_context);
+
+    digitalWrite(LEDR, 0);
+    digitalWrite(LEDG, 1);
+}
 
 void loop() {
 
-  float ax, ay, az; //accelerometer data
-  float gx, gy, gz; //gyroscope data
+    
+    
+    digitalWrite(LEDB, 1);
+    const int16_t *buffer = pv_audio_rec_get_new_buffer();
+    digitalWrite(LEDG, 0);
+    if (buffer) {
+        const pv_status_t status = pv_picovoice_process(handle, buffer);
 
-  static unsigned long startTime = millis();
-  
-  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
-      IMU.readAcceleration(ax, ay, az);
-      IMU.readGyroscope(gx, gy, gz);
 
-      unsigned long timestamp = millis() - startTime;
-      
-      Serial.print(timestamp);
-      Serial.print(",");
-      Serial.print(ax, 6); Serial.print(",");
-      Serial.print(ay, 6); Serial.print(",");
-      Serial.print(az, 6); Serial.print(",");
-      Serial.print(gx, 6); Serial.print(",");
-      Serial.print(gy, 6); Serial.print(",");
-      Serial.println(gz, 6);
-  }
-
-  delay(10); // Adjust sampling rate (100 Hz) //tempo de cooldown entre gestos
-
- 
+        if (status != PV_STATUS_SUCCESS) {
+            Serial.print("Picovoice process failed with ");
+            Serial.println(pv_status_to_string(status));
+            char **message_stack = NULL;
+            int32_t message_stack_depth = 0;
+            pv_get_error_stack(
+                &message_stack,
+                &message_stack_depth);
+            for (int32_t i = 0; i < message_stack_depth; i++) {
+                Serial.println(message_stack[i]);
+            }
+            pv_free_error_stack(message_stack);
+            while (1){
+                digitalWrite(LEDR, 1);
+                delay(1000); // Keep the LED on for 1 second
+                digitalWrite(LEDR, 0);
+            }
+        }
+    }
 }
-
 
