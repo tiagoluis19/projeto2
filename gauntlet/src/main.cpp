2617a897399ffcf4be7b65defbfd07c5307ea97f
tiagoluis19 <tiagoluisfurtado@gmail.com>
2025-06-27 18:19:34 +0100

    fundamental changes

--- gauntlet/src/main.cpp
+++ gauntlet/src/main.cpp
@@ -28,15 +28,18 @@ Neotimer secTimer_10 = Neotimer(10000); //10 seconds timer for user input
 
 
 void setup(){
-    Serial.begin(115200);
+    Serial.begin(9600);
+    analogReadResolution(12); //set the resolution of the analog read to 12 bits
     while (!Serial);
+
+   
 
     if (!IMU.begin()) {
         Serial.println("Failed to initialize IMU!");
         while (1);
     }
-    
-
+     Serial.println(IMU.accelerationSampleRate()); //set the sample rate of the IMU to 100Hz
+    Serial.println(IMU.gyroscopeSampleRate());
 
     pinMode(A1, INPUT); //pointer   finger
     pinMode(A3, INPUT); //middle    finger 
@@ -50,40 +53,35 @@ void setup(){
 
 };
 
+Recorder recorder = {0}; //initial state is idle
 
 void loop(){
 
-int limit;
-int user_time;//in milliseconds
+int limit = 2400;
+int user_time=100;//in milliseconds
 
-Recorder recorder = {0}; //initial state is idle
+
 
     switch (recorder.state){
     case 0: //idle state waits for user input to start recording getures
             //indicated by green LED
-        digitalWrite(LEDG, HIGH); //turn on green LED    
-        if(analogRead(A1)==limit)
+        digitalWrite(LEDG, HIGH); //turn on green LED
+       
+          
+        if(analogRead(A1)<=limit)
             recorder.state = 1; //if pointer finger is up then start the recording session
         break;
 
     case 1: //recording state records the gestures and transmits them via UART
             //indicated by red LED
-        delay(user_time); //delay to give time to the user
+         //delay to give time to the user
 
         
         digitalWrite(LEDR, HIGH); //turn on red LED
-        secTimer_10.start(); //start the timer
+        
         
 
-        if(secTimer_10.done()){
-            secTimer_10.stop();
-            digitalWrite(LEDB, HIGH);
-            digitalWrite(LEDR, LOW);
-            delay(1000);
-            digitalWrite(LEDB, LOW);
-            recorder.state = 0; //return to idle state
-            break; //exit the case
-        }
+        
 
         //read the IMU data
         
@@ -91,19 +89,10 @@ Recorder recorder = {0}; //initial state is idle
         IMU.readAcceleration(ax, ay, az);
         IMU.readGyroscope(gx, gy, gz);  // attention must me in radians
 
-        gx = gx * DEG_TO_RAD; gy = gy * DEG_TO_RAD; gz = gz * DEG_TO_RAD;
+        Serial.print(ax);Serial.print(";");Serial.print(ay);Serial.print(";");Serial.print(az);Serial.print(";");Serial.print(gx);Serial.print(";");Serial.print(gy);Serial.print(";");Serial.print(gz);Serial.println(";");
 
-        deltat = fusion.deltatUpdate();
-
-        fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, deltat); //update the fusion algorithm
-
-        pitch = fusion.getPitch();   // results in degrees   to use radians use getPitchRadians() and so on
-        roll = fusion.getRoll();
-        yaw = fusion.getYaw();
-
-        Serial.print(pitch);Serial.print(";");Serial.print(roll);Serial.print(";");Serial.print(yaw);Serial.println(";");
-
-
+        if(analogRead(A7)<=limit){ //if the user sends the stop command then stop the recording session
+            recorder.state = 0; 
 
 
         break;
@@ -126,5 +115,6 @@ Recorder recorder = {0}; //initial state is idle
     }
 
 };
+}
 
 
